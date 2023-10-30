ed290d1aaf090ff21cbb07eae154facf3e8773db
Clement Masia <clementxmasia@gmail.com>
2023-10-31 01:04:46 +0200

    ActivateQuestion

--- Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/ObjectRotator.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/ObjectRotator.cs
@@ -1,13 +1,67 @@
 using UnityEngine;
+using Photon.Pun;
 
-public class ObjectRotator : MonoBehaviour
+public class ObjectRotator : MonoBehaviourPun, IPunObservable
 {
-    public float rotationSpeed = 30.0f;  // Adjust the rotation speed as needed
+    public float rotationSpeed = 30.0f; // Adjust the rotation speed as needed
     public Vector3 rotationAxis = Vector3.up; // Change the axis of rotation as needed
+
+    private bool isGreen = false; // Tracks if the object is green
 
     void Update()
     {
         // Rotate the object around the specified axis at a constant speed
-        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
+        if (photonView.IsMine)
+        {
+            transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (photonView.IsMine && !isGreen)
+        {
+            // Check if a player has collided with the object
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                // Change the color to green
+                isGreen = true;
+                GetComponent<Renderer>().material.color = Color.green;
+                photonView.RPC("SyncColor", RpcTarget.Others, isGreen);
+            }
+        }
+    }
+
+    [PunRPC]
+    void SyncColor(bool green)
+    {
+        isGreen = green;
+        if (green)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // Sending the rotation data to other players
+            stream.SendNext(transform.rotation);
+            stream.SendNext(isGreen);
+        }
+        else
+        {
+            // Receiving the rotation data from the owner
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+            bool receivedIsGreen = (bool)stream.ReceiveNext();
+
+            if (receivedIsGreen != isGreen)
+            {
+                // Synchronize the color
+                isGreen = receivedIsGreen;
+                GetComponent<Renderer>().material.color = isGreen ? Color.green : Color.white;
+            }
+        }
     }
 }
--- Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/UIMouseInteraction.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/UIMouseInteraction.cs
@@ -4,21 +4,26 @@ using UnityEngine.UI;
 public class UIMouseInteraction : MonoBehaviour
 {
     private string storedLetter = ""; // Initialize as an empty string
-    public GameObject uiElement;
 
     // Update is called once per frame
     void Update()
     {
-        if (!string.IsNullOrEmpty(storedLetter))
+        if (!string.IsNullOrEmpty(storedLetter) && Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                // Trigger the event when a letter is stored and player presses a key
-                Debug.Log("Event triggered with storedLetter: " + storedLetter);
+            // Trigger the event when a letter is stored and player presses a key
+            Debug.Log("Event triggered with storedLetter: " + storedLetter);
 
-                // Reset storedLetter to empty string after the event
-                storedLetter = "";
+            // Try to find a "Questions" script in the scene
+            Question questionsScript = FindObjectOfType<Question>();
+
+            if (questionsScript != null)
+            {
+                // Call the method in the "Questions" script and send the storedLetter as a parameter
+                questionsScript.HandleLetter(storedLetter);
             }
+            
+            // Reset storedLetter to an empty string after the event
+            storedLetter = "";
         }
     }
 
@@ -31,11 +36,17 @@ public class UIMouseInteraction : MonoBehaviour
             storedLetter = other.name;
             Debug.Log("Stored letter: " + storedLetter);
         }
-        if (other.CompareTag("Question") && !string.IsNullOrEmpty(storedLetter))
+        else if (other.CompareTag("Question") && !string.IsNullOrEmpty(storedLetter))
         {
             // The player collided with a "Question" tag and storedLetter has a value
             Debug.Log("Player collided with a question!");
-            uiElement.SetActive(true);
+            Question questionsScript = FindObjectOfType<Question>();
+
+            if (questionsScript != null)
+            {
+                // Call the method in the "Questions" script and send the storedLetter as a parameter
+                questionsScript.HandleLetter(storedLetter);
+            }
 
             // You can trigger an event here, e.g., showing UI or performing an action.
         }
